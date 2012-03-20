33437f9695ca1e5ea624b26ead6f473825f5d8c3
Jezeniel Zapanta <jpzapanta22@gmail.com>
2012-03-20 13:22:33 +0800

    FIXED MERGE

--- web/finals.js
+++ web/finals.js
@@ -3,54 +3,58 @@
  * and open the template in the editor.
  */
 
-
 //Function which appends the contents to the sidebar div
-        function appendContents(elem,elemVal){
-            var sidebar = document.getElementById('sidebar');
-            var len = elemVal.length;
-            elem.innerHTML = "";
-            sidebar.style.display = "block";
-            for(var i=0;i<len;i++){
-                    elem.innerHTML+=elemVal[i];
+function appendContents(elem,elemVal){
+    var sidebar = document.getElementById('sidebar');
+    var len = elemVal.length;
+    var label = ["Flight #/ Time:", "From:", "To:", "Duration:", "Price:"];
+
+    elem.innerHTML = "";
+    sidebar.style.display = "block";
+
+    if(elem.getAttribute("id")== "arrival")
+        elem.innerHTML += "<h4>Arrival</h4>";
+    if(elem.getAttribute("id")== "departure")
+        elem.innerHTML += "<h4>Departure</h4>";
+
+    for(var i=0;i<len;i++){
+        elem.innerHTML+= "<b>" +label[i] + "</b> " + elemVal[i];
+    }
+}
+
+//Function that extracts the row's content based on the radio button clicked
+function extractValues(elemId){
+    var obj = document.getElementById(elemId);
+    var children = obj.childNodes;
+    var arrayValues = [];
+    for(var i = 2;i<children.length;i++){
+      if(children[i].nodeName=='TD'){
+           arrayValues.push("<p>"+children[i].innerHTML+"</p>");
+
+      }
+    }
+    return arrayValues;
+}
 
 
-            }
-
+function traverseElem(obj,elem){
+    var len = obj.length;
+    for(var i = 0;i<len;i++){
+        obj[i].onclick = function(){
+            var val = extractValues(this.value);
+            appendContents(elem,val);
         }
+    }
+}
 
-        //Function that extracts the row's content based on the radio button clicked
-        function extractValues(elemId){
-            var obj = document.getElementById(elemId);
-            var children = obj.childNodes;
-            var arrayValues = [];
-            arrayValues[0] = (elemId.substr(2,1)== '3')? '<h2>Arrival</h2>':'<h2>Departure</h2>';
-            for(var i = 2;i<children.length;i++){
-              if(children[i].nodeName=='TD'){
-                   arrayValues.push("<p>"+children[i].innerHTML+"</p>");
-              }
-            }
-            return arrayValues;
-        }
+function getRadios(){
+        var arrivalDiv = document.getElementById('arrival');
+        var departDiv = document.getElementById('departure');
+        var arrival = document.getElementsByName('flArrival');
+        var departure = document.getElementsByName('flDeparture');
 
+        traverseElem(arrival,arrivalDiv);
+        traverseElem(departure,departDiv);
 
-        function traverseElem(obj,elem){
-            var len = obj.length;
-            for(var i = 0;i<len;i++){
-                obj[i].onclick = function(){
-                    var val = extractValues(this.value);
-                    appendContents(elem,val);
-                }
-            }
-        }
-
-        function getRadios(){
-                var arrivalDiv = document.getElementById('arrival');
-                var departDiv = document.getElementById('departure');
-                var arrival = document.getElementsByName('flArrival');
-                var departure = document.getElementsByName('flDeparture');
-
-                    traverseElem(arrival,arrivalDiv);
-                    traverseElem(departure,departDiv);
-
-        }
-        window.onload = getRadios;
+}
+window.onload = getRadios;
